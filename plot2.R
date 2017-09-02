73ea0d74d866881b6c50babf9304ca7604d68621
PRAGATI0694 <pragatimisra0694@gmail.com>
2017-09-02 16:35:48 +0530

    plot2.R

--- plot2.R
+++ plot2.R
@@ -1,10 +1,8 @@
-dataFile <- "./data/household_power_consumption.txt"
-data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
-
-#str(subSetData)
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]##This subsets the data between the 1st and 2nd feb 2007
+str(subSetData)##it give the subset data
 datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 globalActivePower <- as.numeric(subSetData$Global_active_power)
 png("plot2.png", width=480, height=480)
-plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")##gives the final plot
 dev.off()
