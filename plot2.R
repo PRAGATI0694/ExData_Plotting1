c3d7f3d1833000201f067420af86347e64084553
PRAGATI0694 <pragatimisra0694@gmail.com>
2017-09-02 18:06:25 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -4,5 +4,5 @@ str(subSetData)##it give the subset data
 datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 globalActivePower <- as.numeric(subSetData$Global_active_power)
 png("plot2.png", width=480, height=480)
-plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")##gives the final plot
+a<-plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")##gives the final plot
 dev.off()
